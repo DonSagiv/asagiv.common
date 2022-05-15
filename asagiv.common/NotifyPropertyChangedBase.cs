de0a249a00892cb85960e3e1cd2ff99fe0698ecc
Ari <DonSagiv@users.noreply.github.com>
2022-05-15 10:38:44 -0400

    Revert "Revert "Initial Commit""
    This reverts commit 32c696647a14c8a209477e7d988dd60e32a6f917.

---
+++ asagiv.common/NotifyPropertyChangedBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asagiv.common
+{
+    public abstract class PropertyChangedModel : INotifyPropertyChanged
+    {
+        #region Delegates
+        public event PropertyChangedEventHandler? PropertyChanged;
+        #endregion
+
+        #region Methods
+        protected virtual void RaiseAndSetIfChanged<T>(ref T field, T value, [CallerMemberName]string propertyName = "")
+        {
+            // Raise changed if the original value is null.
+            if(field?.Equals(value) ?? false)
+            {
+                return;
+            }
+
+            field = value;
+
+            RaisePropertyChanged(propertyName);
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
